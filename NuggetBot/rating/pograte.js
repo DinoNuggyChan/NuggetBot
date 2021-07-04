a95c33778160df733ce2fb32a74494ef013c67f6
Nugget~ ♡ <68508971+Oni-Chan-inc@users.noreply.github.com>
2021-07-03 23:18:11 -0400

    Delete NuggetBot directory

--- NuggetBot/rating/pograte.js
+++
@@ -1,9 +0,0 @@
-const sendRate = require("./sendRate.js");
-
-module.exports = {
-    name: 'pograte',
-    description: 'pograte',
-    async execute(client, message, args, command, Discord,) {
-        sendRate(client, message, args, command, Discord, 'Pog');
-    }
-}
